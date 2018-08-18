18232f7ec1dbbfe80d3be9ef0ef084a98f18a2e6
Raphael Thomazella <thomazella9@gmail.com>
2018-08-18 18:30:02 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Test = (
+  <React.Fragment>
+    <p>"well Hello world"</p>
+  </React.Fragment>
+);
+
+ReactDOM.render(Test, document.querySelector("#root"));
