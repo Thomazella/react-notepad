96c6f1bee7fcaafeec7a3b9981ac3a565c7c6251
Raphael Thomazella <thomazella9@gmail.com>
2018-08-26 16:48:27 -0300

    try fix mobile2

--- src/Components/ModalToggle.jsx
+++ src/Components/ModalToggle.jsx
@@ -8,7 +8,7 @@ import NewNote from "./NewNote";
 const WrapperBottom = styled(Flex)`
   justify-content: flex-end;
   align-items: center;
-  position: absolute;
+  position: fixed;
   bottom: 4%;
   width: 86%;
   z-index: 100;
